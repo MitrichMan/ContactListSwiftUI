b456148cb6966673ea35cd8db7d563747623c468
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-05-05 20:20:10 +0800

    Made Person model

---
+++ ContactListSwiftUI/Models/Person.swift
@@ -0,0 +1,62 @@
+//
+//  Person.swift
+//  ContactListSwiftUI
+//
+//  Created by Dmitrii Melnikov on 05.05.2023.
+//
+
+import SwiftUI
+
+struct Person: Identifiable {
+    
+    let id: Int
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    var rows: [String] {
+        [phone, email]
+    }
+}
+
+extension Person {
+    static func getPersonList() -> [Person] {
+        let names = PersonsData.shared.names.shuffled()
+        let surnames = PersonsData.shared.surnames.shuffled()
+        let phones = PersonsData.shared.phones.shuffled()
+        let emails = PersonsData.shared.emails.shuffled()
+        
+        var personList: [Person] = []
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            personList.append(
+                Person(
+                    id: index,
+                    name: names[index],
+                    surname: surnames[index],
+                    phone: phones[index],
+                    email: emails[index]
+                )
+            )
+        }
+        return personList
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
+
