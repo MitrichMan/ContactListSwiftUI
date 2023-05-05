37e5dca597ed47178acc74711fff12c329ee60be
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-05-05 18:32:01 +0800

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Dmitrii Melnikov on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
